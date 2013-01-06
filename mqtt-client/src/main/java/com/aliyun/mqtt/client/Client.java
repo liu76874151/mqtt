7e3f99c91ce43c84aeeffaacab8732c8a7e8aab9
李静 <lij0511@163.com>
2013-01-06 20:13:48 +0800

    commit

---
+++ mqtt-client/src/main/java/com/aliyun/mqtt/client/Client.java
@@ -0,0 +1,11 @@
+package com.aliyun.mqtt.client;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: lijing
+ * Date: 13-1-6
+ * Time: 下午8:12
+ * To change this template use File | Settings | File Templates.
+ */
+public class Client {
+}
